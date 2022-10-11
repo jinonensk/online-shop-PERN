69e1eb994ab96f2c09b1a36e9fb05257cf394392
jinonensk <jinonensk@gmail.com>
2022-10-11 14:25:55 +0700

    chages

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ path: './.env.local' })
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
