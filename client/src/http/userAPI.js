26908c8dc98fc8a105a2268cee0116686efeb497
jinonensk <jinonensk@gmail.com>
2022-10-16 04:21:55 +0700

    add login and registration func

---
+++ client/src/http/userAPI.js
@@ -0,0 +1,26 @@
+import { $authHost, $host } from './index'
+import jwtDecode from 'jwt-decode'
+
+export const registration = async (email, password) => {
+  const { data } = await $host.post('api/user/registration', {
+    email,
+    password,
+    role: 'ADMIN',
+  })
+  localStorage.setItem('token', data.token)
+  return jwtDecode(data.token)
+}
+
+export const login = async (email, password) => {
+  const { data } = await $host.post('api/user/login', {
+    email,
+    password,
+  })
+  localStorage.setItem('token', data.token)
+  return jwtDecode(data.token)
+}
+
+export const check = async () => {
+  const response = await $host.post('api/user/check')
+  return response
+}
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useContext, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { observer } from 'mobx-react-lite'
+
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import { NavLink } from 'react-router-dom'
-import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/constants'
 
-const Auth = () => {
+import { LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from '../utils/constants'
+import { login, registration } from '../http/userAPI'
+import { Context } from '..'
+
+const Auth = observer(() => {
   const location = useLocation()
+  const navigate = useNavigate()
+  const { user } = useContext(Context)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const isLogin = location.pathname === LOGIN_ROUTE
+
+  const handleButtonClick = async () => {
+    let userData
+
+    try {
+      if (isLogin) {
+        userData = await login(email, password)
+      } else {
+        userData = await registration(email, password)
+      }
+      user.setUser(userData)
+      user.setIsAuth(true)
+      navigate(SHOP_ROUTE)
+    } catch (e) {
+      console.debug(e)
+      const message = e.response?.data?.message
+      if (message) alert(message)
+    }
+    console.log(user)
+  }
+
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
@@ -18,8 +49,20 @@ const Auth = () => {
       <Card style={{ width: 600 }} className="p-5">
         <h2 className="m-auto">{isLogin ? 'Авторизция' : 'Регистрация'}</h2>
         <Form>
-          <Form.Control className="mt-3" placeholder="Введите email" />
-          <Form.Control className="mt-3" placeholder="Введите пароль" />
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите email"
+            type="email"
+            value={email}
+            onChange={(evt) => setEmail(evt.target.value)}
+          />
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите пароль"
+            type="password"
+            value={password}
+            onChange={(evt) => setPassword(evt.target.value)}
+          />
           <div className="d-flex justify-content-between align-items-center mt-3 px-3">
             {isLogin ? (
               <div>
@@ -32,7 +75,7 @@ const Auth = () => {
               </div>
             )}
 
-            <Button variant="outline-success">
+            <Button variant="outline-success" onClick={handleButtonClick}>
               {isLogin ? 'Войти' : 'Регистрция'}
             </Button>
           </div>
@@ -40,6 +83,6 @@ const Auth = () => {
       </Card>
     </Container>
   )
-}
+})
 
 export default Auth
